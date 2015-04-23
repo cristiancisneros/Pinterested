96d92ba7be8d938c4252674408c7da2364f98ad3
Cristian Cisneros <computernerdschris@gmail.com>
2015-04-23 07:56:13 +0000

    fixed the missing image issue

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -47,7 +47,7 @@ class PinsController < ApplicationController
     end
 
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 
 
