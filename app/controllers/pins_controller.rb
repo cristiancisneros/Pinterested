db1d81a0b92eef33b6640fe39d1b676fa65d8e01
Cristian Cisneros <computernerdschris@gmail.com>
2015-04-23 02:22:10 +0000

    added associations

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,39 +1,44 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
   respond_to :html
 
   def index
     @pins = Pin.all
-    respond_with(@pins)
   end
-
+  
   def show
-    respond_with(@pin)
   end
 
   def new
-    @pin = Pin.new
-    respond_with(@pin)
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    @pin = current_user_pins_build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: "Pin was successfully created."
+    else
+      render action: 'new'
+    end
   end
 
   def update
-    @pin.update(pin_params)
-    respond_with(@pin)
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was successfully updated."
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+    redirect_to pins_url
   end
 
   private
@@ -45,3 +50,9 @@ class PinsController < ApplicationController
       params.require(:pin).permit(:description)
     end
 end
+
+  def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+end
+  
